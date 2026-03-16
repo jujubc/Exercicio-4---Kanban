0af3a4ca88861cab2ac5e900a22c41802f34ebf7
jujubc <jubsbcorrea@gmail.com>
2026-03-16 18:17:41 -0300

    Exercício Kanban - Feito até o passo 95 (página 60) Juliana B. Corrêa

--- app/src/main/java/com/juliana/task/ui/HomeFragment.kt
+++ app/src/main/java/com/juliana/task/ui/HomeFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.juliana.task.R
+import com.juliana.task.databinding.FragmentHomeBinding
 import com.juliana.task.databinding.FragmentLoginBinding
+import com.juliana.task.ui.adapter.ViewPagerAdapter
+
 
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
 
@@ -20,8 +24,28 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initTabs()
+    }
+
+    private fun initTabs() {
+        val pageAdapter = ViewPagerAdapter(requireActivity())
+        binding.viewPager.adapter = pageAdapter
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_doing)
+        pageAdapter.addFragment(TodoFragment(), R.string.status_task_done)
+
+        binding.viewPager.offscreenPageLimit = pageAdapter.itemCount
+
+        TabLayoutMediator(binding.tabs, binding.viewPager){ tab, position ->
+            tab.text = getString(pageAdapter.getTitle(position))
+        }.attach()
     }
 
     override fun onDestroyView() {
